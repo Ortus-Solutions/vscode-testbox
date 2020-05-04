0b1255cf993c3142e31468c59483037c255d669c
Luis Majano <lmajano@gmail.com>
2020-05-04 17:13:24 -0500

    ### Added
    - TestBox 4 support
    - Formatting
    - More metadata for package.json

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,14 @@ All notable changes to the "vscode-testbox" extension will be documented in this
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.0.0] - 2020-05-04
+
+### Added
+
+- TestBox 4 support
+- Formatting
+- More metadata for package.json
+
 ## [0.2.0] - 2018-05-23
 
 - Jump to spec lists all describe, it, and other blocks for the current file and gives the user a filterable dropdown to select a test to jump to.
--- build.sh
+++ build.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 
-Target[1]="./cbox-coldbox-sublime/snippets/testbox/*.*"
-Target[2]="./cbox-coldbox-sublime/snippets/testing/*.*"
-Target[3]="./cbox-coldbox-sublime/skeletons/test-bdd.sublime-snippet"
-Target[4]="./cbox-coldbox-sublime/skeletons/test-unit.sublime-snippet"
+# Cleanup just in case
+rm -fr ./cbox-coldbox-sublime
+rm -fr ./target
 
+# Clone sublime central data
 git clone https://github.com/lmajano/cbox-coldbox-sublime
+
+npm install
 npm install convert-snippets-to-vscode
 
 mkdir ./target
-for i in {1..4}
-do
-    cp ${Target[i]} ./target
-done
 
+# Copy Snippets
+cp -v ./cbox-coldbox-sublime/snippets/testbox/*.* ./target
+
+# Copy Skeletons
+cp -v ./cbox-coldbox-sublime/skeletons/test-bdd.sublime-snippet ./target
+cp -v ./cbox-coldbox-sublime/skeletons/test-unit.sublime-snippet ./target
+
+# Convert Sublime Snippets to VS Code
 ./node_modules/.bin/snippetToVsCode -s ./target -o ./snippets/snippets.json
 
-npm uninstall convert-snippets-to-vscode
-rm -fr ./node_modules
-rm ./package-lock.json
+#npm uninstall convert-snippets-to-vscode
+
+## Cleanup
+#npm uninstall convert-snippets-to-vscode
 rm -fr ./cbox-coldbox-sublime
 rm -fr ./target
 
