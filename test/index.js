817a08f4f8810df598cd4d1ced849c0ac95fa98a
Mark Drew <mark@distroid.com>
2025-02-27 13:53:35 +0000

    Adding link to the site to show what was done

--- test/index.js
+++ test/index.js
@@ -14,6 +14,7 @@ const testRunner = require('vscode/lib/testrunner');
 
 // You can directly control Mocha options by uncommenting the following lines
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
+/// see: https://code.visualstudio.com/api/working-with-extensions/testing-extension for how to run all of this
 testRunner.configure({
     useColors: true, // colored output from test results
     timeout: 4000
